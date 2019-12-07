fc61060acc17ff2e53ce2bddbbecb3e2716aeb42
Hunter Nemitz <hunter.nemitz@gmail.com>
2019-12-07 02:03:39 -0500

    Cleanup

--- day06-no-tree.py
+++ day06-no-tree.py
@@ -28,21 +28,27 @@ def back_it_up(dtn, lnum, snum, ln, sn):
             if dtn[i][1] == tn1:
                 tn1 = dtn[i][0]
                 break
+
         if tsnum == tlnum:
             for i in range(len(dtn)):
                 if dtn[i][1] == tn2:
                     tn2 = dtn[i][0]
                     break
+
             tsnum -= 1
+
         tlnum -= 1
+
         if tn1 == tn2:
             return tsnum
 
-
 dtn = [(d[:3], d[4:]) for d in data.split('\n')]
+
 p1 = find_children(dtn, 'COM', 1, 'dsasdads')
-p2a, p2b = find_children(dtn, 'COM', 1, 'YOU'), find_children(dtn, 'COM', 1, 'SAN')
-p2c = back_it_up(dtn, nums[0], nums[1], 'YOU', 'SAN')
 print(p1)
+
+find_children(dtn, 'COM', 1, 'YOU')
+find_children(dtn, 'COM', 1, 'SAN')
+p2c = back_it_up(dtn, nums[0], nums[1], 'YOU', 'SAN')
 p2 = nums[0] + nums[1] - 2 - p2c*2
 print(p2)
