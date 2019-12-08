44dd77507f2e4cfd3e2f1f2b28b38b25b94ff009
Hunter Nemitz <hunter.nemitz@gmail.com>
2019-12-08 15:06:28 -0500

    Added day08

---
+++ day08.py
@@ -0,0 +1,43 @@
+from aocd import get_data
+data = [int(d) for d in get_data(day=8)]
+
+def count_numbers(data):
+    start = 0
+    end = 150
+    max_counts = [150, 150, 150]
+    while True:
+        counts = [0, 0, 0]
+        for i in range(start,end):
+            if data[i] == 0:
+                counts[0] += 1
+            elif data[i] == 1:
+                counts[1] += 1
+            elif data[i] == 2:
+                counts[2] += 1
+        if counts[0] < max_counts[0]:
+            max_counts = counts
+        if end == len(data):
+            break
+        start +=150
+        end += 150
+    return max_counts[1]*max_counts[2]
+
+def construct_img(data):
+    master = [[0]*25]*6
+    for d in range(10):
+        l = 150*d
+        for v in range(6):
+            r = 25*v
+            for h in range(25):
+                p = h+r+l
+                print(l,r,p)
+                if data[p] != 2:
+                    master[v][h] = data[p]
+    return master
+
+print(f'P1: {count_numbers(data)}')
+img = construct_img(data)
+for i in img:
+    for j in range(25):
+        print(i[j], end='')
+    print('\n')
